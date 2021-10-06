4609b3c0989af3c34cb0729c2374aead5b9d74b6
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-10-06 15:34:28 -0500

    Test improvements

--- src/main.test.ts
+++ src/main.test.ts
@@ -7,18 +7,30 @@ import fs from 'fs';
 import path from 'path';
 import { reflowLines, stringToLines } from "./main";
 
-function readTestData(name: string): { input: string[]; expected: string; } {
-    const pathStem = path.join(__dirname, '../test-data', name);
-    const input = stringToLines(fs.readFileSync(`${pathStem}.input.adoc`, { encoding: 'utf-8' }));
-    const expected = fs.readFileSync(`${pathStem}.expected.adoc`, { encoding: 'utf-8' });
-    return { input: input, expected: expected };
+function readExpectedFile(name: string): string {
+    const fn = path.join(__dirname, '../test-data', `${name}.expected.adoc`);
+    return fs.readFileSync(fn, { encoding: 'utf-8' });
 }
 
-function readNumberedTestData(name: string, inputNum: number): { input: string[]; expected: string; } {
-    const pathStem = path.join(__dirname, '../test-data', name);
-    const input = stringToLines(fs.readFileSync(`${pathStem}.input.${inputNum}.adoc`, { encoding: 'utf-8' }));
-    const expected = fs.readFileSync(`${pathStem}.expected.adoc`, { encoding: 'utf-8' });
-    return { input: input, expected: expected };
+function readInputData(name: string, inputNum?: number): string[] {
+    const fn = path.join(__dirname, '../test-data', (inputNum == undefined) ? `${name}.input.adoc` : `${name}.input.${inputNum}.adoc`);
+    return stringToLines(fs.readFileSync(fn, { encoding: 'utf-8' }));
+}
+
+class TestData {
+    private stem_: string;
+    private expected_: string;
+    constructor(stem: string) {
+        this.stem_ = stem;
+        this.expected_ = readExpectedFile(this.stem_);
+    }
+    public get expected(): string {
+        return this.expected_;
+    }
+
+    public inputData(inputNum?: number): string[] {
+        return readInputData(this.stem_, inputNum);
+    }
 }
 
 test('first do no harm', () => {
@@ -65,12 +77,20 @@ test('box do no harm', () => {
     expect(reflowLines(input, null)).toEqual(bareInput);
 });
 
-test('box preserve hanging indent', () => {
-    const data = readTestData("preserve-hanging-indent");
-    expect(reflowLines(data.input, null)).toEqual(data.expected);
+describe('box with simpler hanging indent', () => {
+    const data = new TestData("preserve-hanging-indent");
+    test('can combine while preserving the right hanging indent', () => {
+        const input = readInputData("preserve-hanging-indent");
+        expect(reflowLines(data.inputData(), null)).toEqual(data.expected);
+    });
 });
 
-test('box with hanging indent', () => {
-    const data = readNumberedTestData("hanging-indent", 1);
-    expect(reflowLines(data.input, null)).toEqual(data.expected);
+describe('box with hanging indent', () => {
+    const data = new TestData("hanging-indent");
+    test('can be formatted correctly when partially flattened', () => {
+        expect(reflowLines(data.inputData(1), null)).toEqual(data.expected);
+    });
+    test('can be formatted correctly when fully flattened', () => {
+        expect(reflowLines(data.inputData(2), null)).toEqual(data.expected);
+    });
 });
