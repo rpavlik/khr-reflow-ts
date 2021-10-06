715beb7e37616f1cd67bf0049b49f251816be0ff
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-10-06 12:31:36 -0500

    More tests, from OpenXR spec.

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,15 +1,18 @@
 // Copyright (c) 2016-2021, The Khronos Group Inc.
 // Copyright 2021 Collabora, Ltd
 //
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: CC-BY_4.0
 
 import { reflowLines } from "./main";
 
+function stringToInputLines(input: string) {
+    return input.split('\n').map((line) => { return line + '\n'; });
+}
 test('first do no harm', () => {
     let bareInput =
         `This is a first line.
 `;
-    let input = bareInput.split('\n').map((line) => { return line + '\n'; });
+    let input = stringToInputLines(bareInput)
     expect(reflowLines(input, null)).toEqual(bareInput);
 });
 
@@ -18,6 +21,19 @@ test('first do no harm 2', () => {
         `This is a first line.
 This is a second.
 `;
-    let input = bareInput.split('\n').map((line) => { return line + '\n'; });
+    let input = stringToInputLines(bareInput);
     expect(reflowLines(input, null)).toEqual(bareInput);
+});
+
+
+test('normal paragraph', () => {
+    let bareInput =
+        `In each such use, the API major version number, minor version number, and patch version number are packed into a 64-bit integer, referred to as
+basetype:XrVersion, as follows:
+`;
+    let input = stringToInputLines(bareInput);
+    expect(reflowLines(input, null)).toEqual(String.raw`In each such use, the API major version number, minor version number, and
+patch version number are packed into a 64-bit integer, referred to as
+basetype:XrVersion, as follows:
+`);
 });
