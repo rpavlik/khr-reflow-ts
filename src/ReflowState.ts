129e30fb89cfc4f3310bcd326471dbf46f8afa30
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-10-06 13:13:11 -0500

    Extract regexes and test some

--- src/ReflowState.ts
+++ src/ReflowState.ts
@@ -6,82 +6,10 @@
 
 import { Logger } from "tslog";
 import ReflowOptions from "./ReflowOptions";
+import { Regexes } from "./Regexes";
 
 const log: Logger = new Logger({ name: "ReflowState" });
 
-const Regexes = {
-    // Markup that always ends a paragraph
-    //   empty line or whitespace
-    //   [block options]
-    //   [[anchor]]
-    //   //                  comment
-    //   <<<<                page break
-    //   :attribute-setting
-    //   macro-directive::terms
-    //   +                   standalone list item continuation
-    //   label::             labelled list - label must be standalone
-    endPara: /^( *|\[.*\]|\/\/.*|<<<<|:.*|[a-z]+::.*|\+|.*::)$/,
-
-    // Special case of markup ending a paragraph, used to track the current
-    // command/structure. This allows for either OpenXR or Vulkan API path
-    // conventions. Nominally it should use the file suffix defined by the API
-    // conventions (conventions.file_suffix), except that XR uses '.txt' for
-    // generated API include files, not '.adoc' like its other includes.
-    includePat: /include::(?<directory_traverse>((..\/){1,4}|\{INCS-VAR\}\/|\{generated\}\/)(generated\/)?)(?<generated_type>[\w]+)\/(?<category>\w+)\/(?<entity_name>[^./]+).txt[\[][\]]/,
-
-    // Find the first pname: pattern in a Valid Usage statement
-    pnamePat: /pname:(?<param>\w+)/,
-
-    // Markup that's OK in a contiguous paragraph but otherwise passed through
-    //   .anything
-    //   === Section Titles
-    //   image::path_to_image[attributes]  (apparently a single colon is OK but less idiomatic)
-    endParaContinue: /^(\..*|=+ .+|image:.*\[.*\])$/,
-
-    // Markup for block delimiters whose contents *should* be reformatted
-    //   --   (exactly two)  (open block)
-    //   **** (4 or more)    (sidebar block - works best/only? in AsciiDoctor 2)
-    //   ==== (4 or more)    (example block)
-    //   ____ (4 or more)    (quote block)
-    blockReflow: /^(--|[*=_]{4,})$/,
-
-
-    // Markup for block delimiters whose contents should *not* be reformatted
-    //   |=== (3 or more)  (table)
-    //   ++++ (4 or more)  (passthrough block)
-    //   .... (4 or more)  (literal block)
-    //   //// (4 or more)  (comment block)
-    //   ---- (4 or more)  (listing block)
-    //   ```  (3 or more)  (listing block)
-    blockPassthrough: /^(\|={3,}|[`]{3}|[-.+/]{4,})$/,
-
-    // Markup for introducing lists (hanging paragraphs)
-    //   * bullet
-    //     ** bullet
-    //     -- bullet
-    //   . bullet
-    //   :: bullet (no longer supported by asciidoctor 2)
-    //   {empty}:: bullet
-    //   1. list item
-    //   <1> source listing callout
-    beginBullet: /^ *([-*.]+|\{empty\}::|::|[0-9]+[.]|<([0-9]+)>) /,
-
-    // Text that (may) not end sentences
-
-    // A single letter followed by a period, typically a middle initial.
-    endInitial: /^[A-Z]\.$/,
-
-    // An abbreviation, which doesn't (usually) end a line.
-    endAbbrev: /(e\.g|i\.e|c\.f)\.$/i,
-
-    // Explicit Valid Usage list item with one or more leading asterisks
-    // The dotAll (s) is needed to prevent vuPat.exec() from stripping
-    // the trailing newline.
-    vuPat: /^(?<head>  [*]+)( *)(?<tail>.*)/s,
-
-    // Pattern matching leading nested bullet points
-    nestedVuPat: /^  \*\*/,
-};
 
 // Fake block delimiters for "common" VU statements
 const blockCommonReflow = '// Common Valid Usage\n';
---
+++ src/Regexes.test.ts
@@ -0,0 +1,92 @@
+// Copyright (c) 2016-2021, The Khronos Group Inc.
+// Copyright 2021 Collabora, Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { Regexes } from "./Regexes"
+
+
+describe('endPara', () => {
+    test('should match these strings', () => {
+        expect('').toMatch(Regexes.endPara);
+        expect('  ').toMatch(Regexes.endPara);
+        expect('[block options]').toMatch(Regexes.endPara);
+        expect('[[anchor]]').toMatch(Regexes.endPara);
+        expect('// comment').toMatch(Regexes.endPara);
+        // page break
+        expect('<<<<').toMatch(Regexes.endPara);
+        expect(':attribute-setting').toMatch(Regexes.endPara);
+        expect('macrodirective::terms').toMatch(Regexes.endPara);
+        expect('+').toMatch(Regexes.endPara);
+        expect('label::').toMatch(Regexes.endPara);
+    });
+    test('should not match these', () => {
+        expect('asdf').not.toMatch(Regexes.endPara);
+        expect('+5').not.toMatch(Regexes.endPara);
+        // expect('////').not.toMatch(Regexes.endPara);
+    });
+})
+
+describe('endParaContinue', () => {
+    test('should match these strings', () => {
+        expect('.anything').toMatch(Regexes.endParaContinue);
+        expect('=== Section Titles').toMatch(Regexes.endParaContinue);
+        expect('image::path_to_image[attributes]').toMatch(Regexes.endParaContinue);
+        expect('image:path_to_image[attributes]').toMatch(Regexes.endParaContinue);
+    });
+    test('should not match these', () => {
+    });
+})
+
+describe('blockReflow', () => {
+    test('should match these strings', () => {
+        expect('--').toMatch(Regexes.blockReflow);
+        expect('****').toMatch(Regexes.blockReflow);
+        expect('====').toMatch(Regexes.blockReflow);
+        expect('____').toMatch(Regexes.blockReflow);
+    });
+    test('should match these extended strings', () => {
+        expect('*****').toMatch(Regexes.blockReflow);
+        expect('=====').toMatch(Regexes.blockReflow);
+        expect('_____').toMatch(Regexes.blockReflow);
+    });
+
+    test('should not match these', () => {
+        expect('---').not.toMatch(Regexes.blockReflow);
+        expect('----').not.toMatch(Regexes.blockReflow);
+        expect('-').not.toMatch(Regexes.blockReflow);
+        expect('***').not.toMatch(Regexes.blockReflow);
+        expect('===').not.toMatch(Regexes.blockReflow);
+        expect('___').not.toMatch(Regexes.blockReflow);
+    });
+})
+
+describe('blockPassthrough', () => {
+    test('should match these strings', () => {
+        expect('|===').toMatch(Regexes.blockPassthrough); // (3 or more)  (table)
+        expect('++++').toMatch(Regexes.blockPassthrough); // (4 or more)  (passthrough block)
+        expect('....').toMatch(Regexes.blockPassthrough); // (4 or more)  (literal block)
+        expect("////").toMatch(Regexes.blockPassthrough); // (4 or more)  (comment block)
+        expect('----').toMatch(Regexes.blockPassthrough); // (4 or more)  (listing block)
+        expect('```').toMatch(Regexes.blockPassthrough); // (exactly 3)  (listing block)
+    });
+    test('should match these extended strings', () => {
+        expect('|====').toMatch(Regexes.blockPassthrough); // (3 or more)  (table)
+        expect('+++++').toMatch(Regexes.blockPassthrough); // (4 or more)  (passthrough block)
+        expect('.....').toMatch(Regexes.blockPassthrough); // (4 or more)  (literal block)
+        expect("/////").toMatch(Regexes.blockPassthrough); // (4 or more)  (comment block)
+        expect('-----').toMatch(Regexes.blockPassthrough); // (4 or more)  (listing block)
+    });
+    test('should not match these', () => {
+        expect('|==').not.toMatch(Regexes.blockPassthrough); // (3 or more)  (table)
+        expect('+++').not.toMatch(Regexes.blockPassthrough); // (4 or more)  (passthrough block)
+        expect('...').not.toMatch(Regexes.blockPassthrough); // (4 or more)  (literal block)
+        expect("///").not.toMatch(Regexes.blockPassthrough); // (4 or more)  (comment block)
+        expect('---').not.toMatch(Regexes.blockPassthrough); // (4 or more)  (listing block)
+        expect('``').not.toMatch(Regexes.blockPassthrough); //  (exactly 3)  (listing block)
+        expect('````').not.toMatch(Regexes.blockPassthrough);
+
+
+    });
+
+});
