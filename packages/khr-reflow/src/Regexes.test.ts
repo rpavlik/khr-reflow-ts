9c044d713f4b79fb982cbde2faf04878801bac68
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-10-08 14:27:51 -0500

    Fix import in test

--- packages/khr-reflow/src/Regexes.test.ts
+++ packages/khr-reflow/src/Regexes.test.ts
@@ -3,102 +3,102 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { REGEXES } from "./Regexes";
+import REFLOW_REGEXES from "./Regexes";
 
 describe("endPara", () => {
   test("should match these strings", () => {
-    expect("").toMatch(REGEXES.endPara);
-    expect("  ").toMatch(REGEXES.endPara);
-    expect("[block options]").toMatch(REGEXES.endPara);
-    expect("[[anchor]]").toMatch(REGEXES.endPara);
-    expect("// comment").toMatch(REGEXES.endPara);
+    expect("").toMatch(REFLOW_REGEXES.endPara);
+    expect("  ").toMatch(REFLOW_REGEXES.endPara);
+    expect("[block options]").toMatch(REFLOW_REGEXES.endPara);
+    expect("[[anchor]]").toMatch(REFLOW_REGEXES.endPara);
+    expect("// comment").toMatch(REFLOW_REGEXES.endPara);
     // page break
-    expect("<<<<").toMatch(REGEXES.endPara);
-    expect(":attribute-setting").toMatch(REGEXES.endPara);
-    expect("macrodirective::terms").toMatch(REGEXES.endPara);
-    expect("+").toMatch(REGEXES.endPara);
-    expect("label::").toMatch(REGEXES.endPara);
+    expect("<<<<").toMatch(REFLOW_REGEXES.endPara);
+    expect(":attribute-setting").toMatch(REFLOW_REGEXES.endPara);
+    expect("macrodirective::terms").toMatch(REFLOW_REGEXES.endPara);
+    expect("+").toMatch(REFLOW_REGEXES.endPara);
+    expect("label::").toMatch(REFLOW_REGEXES.endPara);
   });
   test("should not match these", () => {
-    expect("asdf").not.toMatch(REGEXES.endPara);
-    expect("+5").not.toMatch(REGEXES.endPara);
+    expect("asdf").not.toMatch(REFLOW_REGEXES.endPara);
+    expect("+5").not.toMatch(REFLOW_REGEXES.endPara);
     // expect('////').not.toMatch(Regexes.endPara);
   });
 });
 
 describe("endParaContinue", () => {
   test("should match these strings", () => {
-    expect(".anything").toMatch(REGEXES.endParaContinue);
-    expect(".Extension Name Formatting\n").toMatch(REGEXES.endParaContinue);
-    expect("=== Section Titles").toMatch(REGEXES.endParaContinue);
-    expect("image::path_to_image[attributes]").toMatch(REGEXES.endParaContinue);
-    expect("image:path_to_image[attributes]").toMatch(REGEXES.endParaContinue);
+    expect(".anything").toMatch(REFLOW_REGEXES.endParaContinue);
+    expect(".Extension Name Formatting\n").toMatch(REFLOW_REGEXES.endParaContinue);
+    expect("=== Section Titles").toMatch(REFLOW_REGEXES.endParaContinue);
+    expect("image::path_to_image[attributes]").toMatch(REFLOW_REGEXES.endParaContinue);
+    expect("image:path_to_image[attributes]").toMatch(REFLOW_REGEXES.endParaContinue);
   });
   test("should not match these", () => {
-    expect("path_to_image[attributes]").not.toMatch(REGEXES.endParaContinue);
-    expect("abc").not.toMatch(REGEXES.endParaContinue);
-    expect("1.").not.toMatch(REGEXES.endParaContinue);
-    expect("e.g.").not.toMatch(REGEXES.endParaContinue);
+    expect("path_to_image[attributes]").not.toMatch(REFLOW_REGEXES.endParaContinue);
+    expect("abc").not.toMatch(REFLOW_REGEXES.endParaContinue);
+    expect("1.").not.toMatch(REFLOW_REGEXES.endParaContinue);
+    expect("e.g.").not.toMatch(REFLOW_REGEXES.endParaContinue);
   });
 });
 
 describe("blockReflow", () => {
   test("should match these strings", () => {
-    expect("--").toMatch(REGEXES.blockReflow);
-    expect("****").toMatch(REGEXES.blockReflow);
-    expect("====").toMatch(REGEXES.blockReflow);
-    expect("____").toMatch(REGEXES.blockReflow);
+    expect("--").toMatch(REFLOW_REGEXES.blockReflow);
+    expect("****").toMatch(REFLOW_REGEXES.blockReflow);
+    expect("====").toMatch(REFLOW_REGEXES.blockReflow);
+    expect("____").toMatch(REFLOW_REGEXES.blockReflow);
   });
   test("should match these extended strings", () => {
-    expect("*****").toMatch(REGEXES.blockReflow);
-    expect("=====").toMatch(REGEXES.blockReflow);
-    expect("_____").toMatch(REGEXES.blockReflow);
+    expect("*****").toMatch(REFLOW_REGEXES.blockReflow);
+    expect("=====").toMatch(REFLOW_REGEXES.blockReflow);
+    expect("_____").toMatch(REFLOW_REGEXES.blockReflow);
   });
 
   test("should not match these", () => {
-    expect("---").not.toMatch(REGEXES.blockReflow);
-    expect("----").not.toMatch(REGEXES.blockReflow);
-    expect("-").not.toMatch(REGEXES.blockReflow);
-    expect("***").not.toMatch(REGEXES.blockReflow);
-    expect("===").not.toMatch(REGEXES.blockReflow);
-    expect("___").not.toMatch(REGEXES.blockReflow);
+    expect("---").not.toMatch(REFLOW_REGEXES.blockReflow);
+    expect("----").not.toMatch(REFLOW_REGEXES.blockReflow);
+    expect("-").not.toMatch(REFLOW_REGEXES.blockReflow);
+    expect("***").not.toMatch(REFLOW_REGEXES.blockReflow);
+    expect("===").not.toMatch(REFLOW_REGEXES.blockReflow);
+    expect("___").not.toMatch(REFLOW_REGEXES.blockReflow);
   });
 });
 
 describe("blockPassthrough", () => {
   test("should match these strings", () => {
-    expect("|===").toMatch(REGEXES.blockPassthrough); // (3 or more)  (table)
-    expect("++++").toMatch(REGEXES.blockPassthrough); // (4 or more)  (passthrough block)
-    expect("....").toMatch(REGEXES.blockPassthrough); // (4 or more)  (literal block)
-    expect("////").toMatch(REGEXES.blockPassthrough); // (4 or more)  (comment block)
-    expect("----").toMatch(REGEXES.blockPassthrough); // (4 or more)  (listing block)
-    expect("```").toMatch(REGEXES.blockPassthrough); // (exactly 3)  (listing block)
+    expect("|===").toMatch(REFLOW_REGEXES.blockPassthrough); // (3 or more)  (table)
+    expect("++++").toMatch(REFLOW_REGEXES.blockPassthrough); // (4 or more)  (passthrough block)
+    expect("....").toMatch(REFLOW_REGEXES.blockPassthrough); // (4 or more)  (literal block)
+    expect("////").toMatch(REFLOW_REGEXES.blockPassthrough); // (4 or more)  (comment block)
+    expect("----").toMatch(REFLOW_REGEXES.blockPassthrough); // (4 or more)  (listing block)
+    expect("```").toMatch(REFLOW_REGEXES.blockPassthrough); // (exactly 3)  (listing block)
   });
   test("should match these extended strings", () => {
-    expect("|====").toMatch(REGEXES.blockPassthrough); // (3 or more)  (table)
-    expect("+++++").toMatch(REGEXES.blockPassthrough); // (4 or more)  (passthrough block)
-    expect(".....").toMatch(REGEXES.blockPassthrough); // (4 or more)  (literal block)
-    expect("/////").toMatch(REGEXES.blockPassthrough); // (4 or more)  (comment block)
-    expect("-----").toMatch(REGEXES.blockPassthrough); // (4 or more)  (listing block)
+    expect("|====").toMatch(REFLOW_REGEXES.blockPassthrough); // (3 or more)  (table)
+    expect("+++++").toMatch(REFLOW_REGEXES.blockPassthrough); // (4 or more)  (passthrough block)
+    expect(".....").toMatch(REFLOW_REGEXES.blockPassthrough); // (4 or more)  (literal block)
+    expect("/////").toMatch(REFLOW_REGEXES.blockPassthrough); // (4 or more)  (comment block)
+    expect("-----").toMatch(REFLOW_REGEXES.blockPassthrough); // (4 or more)  (listing block)
   });
   test("should not match these", () => {
-    expect("|==").not.toMatch(REGEXES.blockPassthrough); // (3 or more)  (table)
-    expect("+++").not.toMatch(REGEXES.blockPassthrough); // (4 or more)  (passthrough block)
-    expect("...").not.toMatch(REGEXES.blockPassthrough); // (4 or more)  (literal block)
-    expect("///").not.toMatch(REGEXES.blockPassthrough); // (4 or more)  (comment block)
-    expect("---").not.toMatch(REGEXES.blockPassthrough); // (4 or more)  (listing block)
-    expect("``").not.toMatch(REGEXES.blockPassthrough); //  (exactly 3)  (listing block)
-    expect("````").not.toMatch(REGEXES.blockPassthrough);
+    expect("|==").not.toMatch(REFLOW_REGEXES.blockPassthrough); // (3 or more)  (table)
+    expect("+++").not.toMatch(REFLOW_REGEXES.blockPassthrough); // (4 or more)  (passthrough block)
+    expect("...").not.toMatch(REFLOW_REGEXES.blockPassthrough); // (4 or more)  (literal block)
+    expect("///").not.toMatch(REFLOW_REGEXES.blockPassthrough); // (4 or more)  (comment block)
+    expect("---").not.toMatch(REFLOW_REGEXES.blockPassthrough); // (4 or more)  (listing block)
+    expect("``").not.toMatch(REFLOW_REGEXES.blockPassthrough); //  (exactly 3)  (listing block)
+    expect("````").not.toMatch(REFLOW_REGEXES.blockPassthrough);
   });
 });
 
 describe("endAbbrev", () => {
   test("should match these strings", () => {
-    expect("e.g.").toMatch(REGEXES.endAbbrev);
-    expect("i.e.").toMatch(REGEXES.endAbbrev);
-    expect("c.f.").toMatch(REGEXES.endAbbrev);
+    expect("e.g.").toMatch(REFLOW_REGEXES.endAbbrev);
+    expect("i.e.").toMatch(REFLOW_REGEXES.endAbbrev);
+    expect("c.f.").toMatch(REFLOW_REGEXES.endAbbrev);
   });
   test("should not match these", () => {
-    expect("e.e.").not.toMatch(REGEXES.endAbbrev);
+    expect("e.e.").not.toMatch(REFLOW_REGEXES.endAbbrev);
   });
 });
